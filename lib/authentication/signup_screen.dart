c1bd10954c0137bbaa4cf83f222942c6446fb9fb
Lakheras99 <49496887+Lakheras99@users.noreply.github.com>
2024-03-18 18:50:54 +0530

    third commit

---
+++ lib/authentication/signup_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset('assets/images/logo.png'),
+              const Text(
+                'Create a user\'s account',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.all(22),
+                  child: Column(
+                    children: [
+                      TextField(decoration: InputDecoration(
+                        enabledBorder: true,
+                        border: OutlineInputBorder(),
+                      ),
+                        controller: nameTextEditingController,
+                        keyboardType: TextInputType.text,
+
+
+                      ),
+                    ],
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
