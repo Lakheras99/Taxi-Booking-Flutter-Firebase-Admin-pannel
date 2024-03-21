f922c18dac4c231bb532c04ca232e5fdba34448d
Lakheras99 <49496887+Lakheras99@users.noreply.github.com>
2024-03-22 00:16:18 +0530

    third commit

---
+++ lib/authentication/onboarding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Onboard extends StatelessWidget {
+  const Onboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
